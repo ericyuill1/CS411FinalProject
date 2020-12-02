b0f28cda610f56a85a7767bba39f74273ac59a4a
Teja Kotha <tkotha2@illinois.edu>
2020-12-02 04:16:14 -0600

    elo done

--- app.py
+++ app.py
@@ -6,15 +6,124 @@ Created on Sat Jul 25 12:02:51 2020
 
 from flask import Flask, render_template, request, flash, redirect,url_for, jsonify, session 
 from flask import Response,send_file
+import json
 
 app = Flask(__name__)
 
 import rds_db as db
+
+
 @app.route('/', methods=['get'])
 @app.route('/index', methods=['get', 'post'])
 def index():
     return render_template('index.html')
 
+class PlayerHistory:
+    def __init__(self,_id,name,elo):
+        self.id = _id
+        self.name = name
+        self.elo = elo
+        self.set_history = set()
+    def __str__(self):
+        return f"{self.name}: {self.elo}"
+
+class Sets:
+    def __init__(self,entrants, winner_id, idA, idB, Ascore, Bscore, bracket):
+        self.entrants = entrants
+        self.winner_id = winner_id
+        self.idA = idA
+        self.idB = idB
+        self.Ascore = Ascore
+        self.Bscore = Bscore
+        self.bracket = bracket
+
+@app.route('/rankings', methods=['get'])
+def eloRankings():
+    pgr_player_tourneys = set()
+    pgr50 = dict(db.get_pgr50())
+    pgr50ids = list(pgr50.keys())
+    setdata = db.get_sets_by_list_of_player_ids(pgr50ids)
+    for s in setdata:
+        pgr_player_tourneys.add(s[0])
+    pgr_tourney_entrants = dict(db.get_entrants_by_list_of_tournaments(pgr_player_tourneys))
+    player_ids = dict(db.get_gamertag_by_idlist(list(pgr50ids)))
+    player_histories = {}
+    sets = set()
+    bracket = -1
+    initeloA = initeloB = -1
+    for s in setdata:
+        # print(s)
+        if s[0] not in pgr_tourney_entrants:
+            continue
+        entrants = pgr_tourney_entrants[s[0]]
+        snippet = s[6][2:5]
+        if 'L' in snippet or 'GFR' in snippet:
+            bracket = 0
+        elif 'W' in snippet or 'GF' in snippet:
+            bracket = 1
+        else:
+            continue
+        if 1 <= pgr50[s[2]] <= 10:
+            initeloA = 2000
+        elif 11 <= pgr50[s[2]] <= 20:
+            initeloA = 1900
+        elif 21 <= pgr50[s[2]] <= 30:
+            initeloA = 1800
+        elif 31 <= pgr50[s[2]] <= 40:
+            initeloA = 1700
+        elif 41 <= pgr50[s[2]] <= 50:
+            initeloA = 1600
+        else:
+            initeloA = 1500
+        if 1 <= pgr50[s[3]] <= 10:
+            initeloB = 2000
+        elif 11 <= pgr50[s[3]] <= 20:
+            initeloB = 1900
+        elif 21 <= pgr50[s[3]] <= 30:
+            initeloB = 1800
+        elif 31 <= pgr50[s[3]] <= 40:
+            initeloB = 1700
+        elif 41 <= pgr50[s[3]] <= 50:
+            initeloB = 1600
+        else:
+            initeloB = 1500
+        if s[2] not in player_histories:
+            player_histories[s[2]] = PlayerHistory(s[2], player_ids[s[2]], initeloA)
+            player_histories[s[2]].set_history.add(Sets(entrants,s[1],s[2],s[3],s[4],s[5],bracket))
+        else:
+            player_histories[s[2]].set_history.add(Sets(entrants,s[1],s[2],s[3],s[4],s[5],bracket))
+
+        if s[3] not in player_histories:
+            player_histories[s[3]] = PlayerHistory(s[3], player_ids[s[3]], initeloB)
+            player_histories[s[3]].set_history.add(Sets(entrants,s[1],s[2],s[3],s[4],s[5],bracket))
+        else:
+            player_histories[s[2]].set_history.add(Sets(entrants,s[1],s[2],s[3],s[4],s[5],bracket))
+    
+        sets.add(Sets(entrants,s[1],s[2],s[3],s[4],s[5],bracket))
+
+    for currset in sets:
+        elo1 = player_histories[currset.idA].elo
+        elo2 = player_histories[currset.idB].elo
+        n = currset.entrants
+        R = 1 if currset.winner_id == currset.idA else 0
+        s1 = currset.Ascore
+        s2 = currset.Bscore
+        b = currset.bracket
+        w1 = R
+        h1 = len(player_histories[currset.idA].set_history)
+
+        newElo1 = elo1 + 20 * (1 + b * 0.5 * w1) * (min(5/h1 + 0.4, 2)) * (1.95 + (-1.82 / (1 + (n/1040.56) ** 1.762))) * ((abs(s1-s2)) ** 0.9) * (R - (1/(1 + (10) ** (-1 * ((elo1 - elo2) / 400)))))
+
+        player_histories[currset.idA].elo = newElo1
+
+    final = sorted(player_histories.values(), key=lambda x: x.elo, reverse=True)
+    final2 = {}
+    for item in final:
+        final2[item.name] = item.elo
+
+    return jsonify(final2)
+
+    return render_template('index.html')
 
 def handle_request(request):
     # print(request.form)
--- rds_db.py
+++ rds_db.py
@@ -3,14 +3,13 @@
 Created on Sat Jul 25 13:34:18 2020
 @author: hp
 """
-
-import pymysql
+import pymysql, json
 conn = pymysql.connect(
     host = "panda-local.cmcngdzhrbgb.us-east-2.rds.amazonaws.com",
     port = 3306,
     user = "admin",
     password = "advaitejaeric",
-    database = "smash"
+    database = "teja"
 )
 
 #Table Creation
@@ -28,37 +27,68 @@ get_player(playerId)
 '''
 
 
-def get_player_by_gamertag(tag):
-    query = f"SELECT * FROM Player WHERE gamer_tag = '{tag}'"
-    cur=conn.cursor()
+# def get_player_by_gamertag(tag):
+#     query = f"SELECT * FROM Player WHERE gamer_tag = '{tag}'"
+#     cur=conn.cursor()
+#     cur.execute(query)
+#     details = cur.fetchall()
+#     return details
+
+# def insert_player_by_gamertag(tag):
+#     query = f"INSERT INTO Player (gamer_tag,scraped) VALUES ('{tag}', 'F')"
+#     cur=conn.cursor()
+#     cur.execute(query)
+#     conn.commit()
+#     return
+
+# def update_player_by_gamertag(old_tag, new_tag):
+#     query = f"UPDATE Player SET gamer_tag = '{new_tag}' WHERE gamer_tag = '{old_tag}'"
+#     cur = conn.cursor()
+#     cur.execute(query)
+#     conn.commit()
+#     return 
+
+# def delete_player_by_gamertag(tag):
+#     query = f"DELETE FROM Player WHERE gamer_tag = '{tag}' AND scraped = 'F'"
+#     cur = conn.cursor()
+#     cur.execute(query)
+#     conn.commit()
+#     return
+
+# def get_players_by_placement(placement):
+#     query = f"SELECT * FROM Player P NATURAL JOIN EventStanding E NATURAL JOIN TournamentEvent T WHERE placement={placement} AND P.player_id = E.player_id AND T.tournament_id = E.tournament_id GROUP BY placement"
+#     # query = f"SELECT gamer_tag FROM Player P JOIN EventStanding E JOIN TournamentEvent T WHERE placement={placement} AND P.player_id = E.player_id AND T.tournament_id = E.tournament_id GROUP BY placement"
+#     cur = conn.cursor()
+#     cur.execute(query)
+#     details = cur.fetchall()
+#     return details
+
+def get_pgr50():
+    query = f"select by_id from ranking_seasons where season = 2 and ranking_name = 'PGRU'"
+    cur = conn.cursor()
+    cur.execute(query)
+    details = cur.fetchall()
+    return json.loads(details[0][0])
+
+def get_sets_by_list_of_player_ids(player_list):
+    player_tups = tuple(player_list)
+    query = f"select tournament_key,winner_id,p1_id,p2_id,p1_score,p2_score,location_names from sets where p1_id in {player_tups} and p2_id in {player_tups} and best_of is not null and p1_score >= 0 and p2_score >= 0"
+    cur = conn.cursor()
     cur.execute(query)
     details = cur.fetchall()
     return details
 
-def insert_player_by_gamertag(tag):
-    query = f"INSERT INTO Player (gamer_tag,scraped) VALUES ('{tag}', 'F')"
-    cur=conn.cursor()
-    cur.execute(query)
-    conn.commit()
-    return
-
-def update_player_by_gamertag(old_tag, new_tag):
-    query = f"UPDATE Player SET gamer_tag = '{new_tag}' WHERE gamer_tag = '{old_tag}'"
+def get_entrants_by_list_of_tournaments(tourney_list):
+    tourney_tups = tuple(tourney_list)
+    query = f"select tournament_info.key, entrants from tournament_info where tournament_info.key in {tourney_tups}"
     cur = conn.cursor()
     cur.execute(query)
-    conn.commit()
-    return 
+    details = cur.fetchall()
+    return details
 
-def delete_player_by_gamertag(tag):
-    query = f"DELETE FROM Player WHERE gamer_tag = '{tag}' AND scraped = 'F'"
-    cur = conn.cursor()
-    cur.execute(query)
-    conn.commit()
-    return
-
-def get_players_by_placement(placement):
-    query = f"SELECT * FROM Player P NATURAL JOIN EventStanding E NATURAL JOIN TournamentEvent T WHERE placement={placement} AND P.player_id = E.player_id AND T.tournament_id = E.tournament_id GROUP BY placement"
-    # query = f"SELECT gamer_tag FROM Player P JOIN EventStanding E JOIN TournamentEvent T WHERE placement={placement} AND P.player_id = E.player_id AND T.tournament_id = E.tournament_id GROUP BY placement"
+def get_gamertag_by_idlist(_idlist):
+    _id_tups = tuple(_idlist)
+    query = f"select player_id, tag from players where player_id in {_id_tups}"
     cur = conn.cursor()
     cur.execute(query)
     details = cur.fetchall()
